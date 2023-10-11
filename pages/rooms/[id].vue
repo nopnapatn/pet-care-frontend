415d735ba03ef82098f6dabc3aea95d362d1c8de
Nopnapat Norasri <neuw@Nopnapats-MacBook-Pro.local>
2023-10-11 17:03:39 +0700

    style(.): fix responsive

--- pages/rooms/[id].vue
+++ pages/rooms/[id].vue
@@ -1,15 +1,13 @@
 <template>
   <section>
     <div class="">
-      <!-- Contact Us -->
       <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         <div class="max-w-2xl lg:max-w-5xl mx-auto">
-          <div class="mt-12 grid items-center lg:grid-cols-2 gap-6 lg:gap-16">
-            <div class="divide-y divide-gray-200 dark:divide-gray-800">
-              <!-- Icon Block -->
+          <div class="mt-12 grid lg:grid-cols-2 gap-6 lg:gap-16">
+            <div class="divide-y divide-gray-200">
               <div class="flex gap-x-7 py-6">
                 <svg
-                  class="flex-shrink-0 w-6 h-6 mt-1.5 text-gray-800 dark:text-gray-200"
+                  class="flex-shrink-0 w-6 h-6 mt-1.5 text-gray-800"
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
@@ -24,41 +22,23 @@
                   />
                 </svg>
                 <div>
-                  <h3 class="font-semibold text-gray-800 dark:text-gray-200">
-                    Knowledgebase
+                  <h3 class="font-semibold text-gray-800">
+                    {{ roomType.title }}
                   </h3>
                   <p class="mt-1 text-sm text-gray-500">
                     We're here to help with any questions or code.
                   </p>
-                  <a
-                    class="mt-2 inline-flex items-center gap-x-2 text-sm font-medium text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200"
-                    href="#"
+                  <div
+                    class="mt-2 inline-flex items-center gap-x-2 text-sm font-medium text-gray-600 hover:text-gray-800"
                   >
-                    Contact support
-                    <svg
-                      class="w-2.5 h-2.5 transition ease-in-out group-hover:translate-x-1"
-                      width="16"
-                      height="16"
-                      viewBox="0 0 16 16"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M0.975821 6.92249C0.43689 6.92249 -3.50468e-07 7.34222 -3.27835e-07 7.85999C-3.05203e-07 8.37775 0.43689 8.79749 0.975821 8.79749L12.7694 8.79748L7.60447 13.7596C7.22339 14.1257 7.22339 14.7193 7.60447 15.0854C7.98555 15.4515 8.60341 15.4515 8.98449 15.0854L15.6427 8.68862C16.1191 8.23098 16.1191 7.48899 15.6427 7.03134L8.98449 0.634573C8.60341 0.268455 7.98555 0.268456 7.60447 0.634573C7.22339 1.00069 7.22339 1.59428 7.60447 1.9604L12.7694 6.92248L0.975821 6.92249Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </a>
+                    {{ formData.nights }} Nights
+                  </div>
                 </div>
               </div>
-              <!-- End Icon Block -->
 
-              <!-- Icon Block -->
               <div class="flex gap-x-7 py-6">
                 <svg
-                  class="flex-shrink-0 w-6 h-6 mt-1.5 text-gray-800 dark:text-gray-200"
+                  class="flex-shrink-0 w-6 h-6 mt-1.5 text-gray-800"
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
@@ -66,207 +46,78 @@
                   viewBox="0 0 16 16"
                 >
                   <path
-                    d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+                    d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
                   />
                   <path
-                    d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"
+                    d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"
                   />
                 </svg>
                 <div>
-                  <h3 class="font-semibold text-gray-800 dark:text-gray-200">
-                    FAQ
-                  </h3>
+                  <h3 class="font-semibold text-gray-800">Contact details</h3>
                   <p class="mt-1 text-sm text-gray-500">
-                    Search our FAQ for answers to anything you might ask.
+                    We're here to help with any questions or code.
                   </p>
-                  <a
-                    class="mt-2 inline-flex items-center gap-x-2 text-sm font-medium text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200"
-                    href="#"
+                  <div
+                    class="mt-2 inline-flex items-center gap-x-2 text-sm font-medium text-gray-600 hover:text-gray-800"
                   >
-                    Visit FAQ
-                    <svg
-                      class="w-2.5 h-2.5 transition ease-in-out group-hover:translate-x-1"
-                      width="16"
-                      height="16"
-                      viewBox="0 0 16 16"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M0.975821 6.92249C0.43689 6.92249 -3.50468e-07 7.34222 -3.27835e-07 7.85999C-3.05203e-07 8.37775 0.43689 8.79749 0.975821 8.79749L12.7694 8.79748L7.60447 13.7596C7.22339 14.1257 7.22339 14.7193 7.60447 15.0854C7.98555 15.4515 8.60341 15.4515 8.98449 15.0854L15.6427 8.68862C16.1191 8.23098 16.1191 7.48899 15.6427 7.03134L8.98449 0.634573C8.60341 0.268455 7.98555 0.268456 7.60447 0.634573C7.22339 1.00069 7.22339 1.59428 7.60447 1.9604L12.7694 6.92248L0.975821 6.92249Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </a>
+                    Name <br />
+                    {{ auth.user.firstName }} {{ auth.user.lastName }}
+                    <br /><br />
+                    Email <br />
+                    {{ auth.user.email }} <br />
+                  </div>
                 </div>
               </div>
-              <!-- End Icon Block -->
-
-              <!-- Icon Block -->
-              <div class="flex gap-x-7 py-6">
-                <svg
-                  class="flex-shrink-0 w-6 h-6 mt-1.5 text-gray-800 dark:text-gray-200"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M6 9a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3A.5.5 0 0 1 6 9zM3.854 4.146a.5.5 0 1 0-.708.708L4.793 6.5 3.146 8.146a.5.5 0 1 0 .708.708l2-2a.5.5 0 0 0 0-.708l-2-2z"
-                  />
-                  <path
-                    d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h12z"
-                  />
-                </svg>
-                <div>
-                  <h3 class="font-semibold text-gray-800 dark:text-gray-200">
-                    Developer APIs
-                  </h3>
-                  <p class="mt-1 text-sm text-gray-500">
-                    Check out our development quickstart guide.
-                  </p>
-                  <a
-                    class="mt-2 inline-flex items-center gap-x-2 text-sm font-medium text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200"
-                    href="#"
-                  >
-                    Contact sales
-                    <svg
-                      class="w-2.5 h-2.5 transition ease-in-out group-hover:translate-x-1"
-                      width="16"
-                      height="16"
-                      viewBox="0 0 16 16"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M0.975821 6.92249C0.43689 6.92249 -3.50468e-07 7.34222 -3.27835e-07 7.85999C-3.05203e-07 8.37775 0.43689 8.79749 0.975821 8.79749L12.7694 8.79748L7.60447 13.7596C7.22339 14.1257 7.22339 14.7193 7.60447 15.0854C7.98555 15.4515 8.60341 15.4515 8.98449 15.0854L15.6427 8.68862C16.1191 8.23098 16.1191 7.48899 15.6427 7.03134L8.98449 0.634573C8.60341 0.268455 7.98555 0.268456 7.60447 0.634573C7.22339 1.00069 7.22339 1.59428 7.60447 1.9604L12.7694 6.92248L0.975821 6.92249Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </a>
-                </div>
-              </div>
-              <!-- End Icon Block -->
-
-              <!-- Icon Block -->
-              <div class="flex gap-x-7 py-6">
-                <svg
-                  class="flex-shrink-0 w-6 h-6 mt-1.5 text-gray-800 dark:text-gray-200"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"
-                  />
-                </svg>
-                <div>
-                  <h3 class="font-semibold text-gray-800 dark:text-gray-200">
-                    Contact us by email
-                  </h3>
-                  <p class="mt-1 text-sm text-gray-500">
-                    If you wish to write us an email instead please use
-                  </p>
-                  <a
-                    class="mt-2 inline-flex items-center gap-x-2 text-sm font-medium text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200"
-                    href="#"
-                  >
-                    example@site.com
-                  </a>
-                </div>
-              </div>
-              <!-- End Icon Block -->
             </div>
-            <!-- Card -->
+
             <div
               class="bg-white shadow flex flex-col border rounded-xl p-4 sm:p-6 lg:p-8"
             >
-              <h2
-                class="mb-8 text-xl font-semibold text-gray-800 dark:text-gray-200"
-              >
-                Fill in the form
+              <h2 class="mb-8 text-xl font-semibold text-gray-800">
+                Price details
               </h2>
 
-              <form>
+              <form @submit.prevent="onSubmit()">
                 <div class="grid gap-4">
-                  <!-- Grid -->
-                  <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                    <div>
-                      <label for="hs-firstname-contacts-1" class="sr-only"
-                        >First Name</label
-                      >
-                      <input
-                        type="text"
-                        name="hs-firstname-contacts-1"
-                        id="hs-firstname-contacts-1"
-                        class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
-                        placeholder="First Name"
-                      />
-                    </div>
-
-                    <div>
-                      <label for="hs-lastname-contacts-1" class="sr-only"
-                        >Last Name</label
-                      >
-                      <input
-                        type="text"
-                        name="hs-lastname-contacts-1"
-                        id="hs-lastname-contacts-1"
-                        class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
-                        placeholder="Last Name"
-                      />
-                    </div>
+                  <div>
+                    <VueDatePicker
+                      v-model="date"
+                      range
+                      format="dd/M/yyyy"
+                      :enable-time-picker="false"
+                      disable-year-select
+                      auto-apply
+                      placeholder="Select date"
+                      :min-date="
+                        new Date(new Date().setDate(new Date().getDate() + 1))
+                      "
+                      :max-date="
+                        new Date(new Date().setDate(new Date().getDate() + 30))
+                      "
+                      required
+                    ></VueDatePicker>
                   </div>
-                  <!-- End Grid -->
 
                   <div>
-                    <label for="hs-email-contacts-1" class="sr-only"
-                      >Email</label
-                    >
                     <input
-                      type="email"
-                      name="hs-email-contacts-1"
-                      id="hs-email-contacts-1"
-                      autocomplete="email"
-                      class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
-                      placeholder="Email"
+                      type="number"
+                      placeholder="Amount of your pets"
+                      min="0"
+                      :max="roomType.max_pets"
+                      v-model="formData.petsAmount"
+                      class="input input-bordered w-full max-w-xs"
                     />
                   </div>
 
                   <div>
-                    <label for="hs-phone-number-1" class="sr-only"
-                      >Phone Number</label
-                    >
-                    <input
-                      type="text"
-                      name="hs-phone-number-1"
-                      id="hs-phone-number-1"
-                      class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
-                      placeholder="Phone Number"
-                    />
-                  </div>
-
-                  <div>
-                    <label for="hs-about-contacts-1" class="sr-only"
-                      >Details</label
-                    >
                     <textarea
-                      id="hs-about-contacts-1"
-                      name="hs-about-contacts-1"
-                      rows="4"
-                      class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
-                      placeholder="Details"
+                      v-model="formData.ownerInstruction"
+                      cols="30"
+                      rows="10"
+                      placeholder="Tell Staff about your pets instruction"
                     ></textarea>
                   </div>
                 </div>
-                <!-- End Grid -->
 
                 <div class="mt-4 grid">
                   <button
@@ -276,70 +127,11 @@
                     Booking
                   </button>
                 </div>
-
-                <div class="mt-3 text-center">
-                  <p class="text-sm text-gray-500">
-                    We'll get back to you in 1-2 business days.
-                  </p>
-                </div>
               </form>
             </div>
-            <!-- End Card -->
           </div>
         </div>
       </div>
-      <!-- End Contact Us -->
-
-      <form @submit.prevent="onSubmit()">
-        <div class="flex-col">
-          <h1>Room ID: {{ route.params.id }}</h1>
-          <h1>
-            {{ roomType.title }}
-          </h1>
-          <h1>Available amount: {{ roomType.available_amount }}</h1>
-          <h1>Price: {{ roomType.price }}</h1>
-          <h1>{{ formData.nights }} / Nights</h1>
-        </div>
-
-        <!-- Start Datepicker -->
-        <VueDatePicker
-          v-model="date"
-          range
-          format="dd/M/yyyy"
-          :enable-time-picker="false"
-          disable-year-select
-          auto-apply
-          placeholder="Select date"
-          :min-date="new Date(new Date().setDate(new Date().getDate() + 1))"
-          :max-date="new Date(new Date().setDate(new Date().getDate() + 30))"
-          required
-        ></VueDatePicker>
-
-        <!-- End Datepicker -->
-        <!-- Owner Instruction -->
-        <textarea
-          v-model="formData.ownerInstruction"
-          cols="30"
-          rows="10"
-          placeholder="Tell Staff about your pets instruction"
-        ></textarea>
-        <!-- End Owner Instruction -->
-        <!-- Amount -->
-        <input
-          type="number"
-          placeholder="Amount of your pets"
-          min="0"
-          :max="roomType.max_pets"
-          v-model="formData.petsAmount"
-          class="input input-bordered w-full max-w-xs"
-        />
-        <!-- End Amount -->
-        <TheButtonAuth
-          colort="bg-primary-green-100"
-          colorb="bg-primary-green-200"
-          >Submit
-        </TheButtonAuth>
-      </form>
     </div>
   </section>
 </template>
@@ -347,6 +139,9 @@
 <script setup lang="ts">
 import VueDatePicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
+import { useAuthStore } from "~/stores/useAuthStore";
+
+const auth = useAuthStore();
 
 const route = useRoute();
 const { data: roomType, error } = await useMyFetch<any>(
