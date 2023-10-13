52285794649bc7642dc8196873085bb9a6bf3798
Kchanit <chanitaphon.l@ku.th>
2023-10-14 00:10:21 +0700

    feat: add nuxt/ui table

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -433,6 +433,35 @@
                           </div>
                         </a>
                       </td>
+                      <td class="h-px w-px whitespace-nowrap">
+                        <a class="block" href="javascript:;">
+                          <button
+                            class="px-6 py-1.5"
+                            @click.prevent="handleCheckOut()"
+                          >
+                            <div
+                              class="py-1 px-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white"
+                            >
+                              <svg
+                                class="w-4 h-4"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="currentColor"
+                                viewBox="0 0 16 16"
+                              >
+                                <path
+                                  d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v12a.5.5 0 0 1-.053.224l-.5 1a.5.5 0 0 1-.8.13L13 14.707l-.646.647a.5.5 0 0 1-.708 0L11 14.707l-.646.647a.5.5 0 0 1-.708 0L9 14.707l-.646.647a.5.5 0 0 1-.708 0L7 14.707l-.646.647a.5.5 0 0 1-.708 0L5 14.707l-.646.647a.5.5 0 0 1-.708 0L3 14.707l-.646.647a.5.5 0 0 1-.801-.13l-.5-1A.5.5 0 0 1 1 14V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zm.217 1.338L2 2.118v11.764l.137.274.51-.51a.5.5 0 0 1 .707 0l.646.647.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.509.509.137-.274V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51z"
+                                />
+                                <path
+                                  d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"
+                                />
+                              </svg>
+                              Check out
+                            </div>
+                          </button>
+                        </a>
+                      </td>
                     </tr>
                   </tbody>
                 </table>
@@ -787,42 +816,50 @@ const { data: bookingOrders, error } = await useMyFetch<any>(
   {}
 );
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const rows = document.querySelectorAll("tr[data-toggle='modal']");
+async function handleCheckOut() {
+  console.log("check out");
+  const { data, error } = await useMyFetch<any>("booking-orders/check-out", {});
+}
 
-//   rows.forEach((row) => {
-//     row.addEventListener("click", function () {
-//       const modal = document.querySelector("#booking-order-modal");
-
-//       // Get data from the clicked row's data attributes
-//       const bookingOrderId = row.getAttribute("data-booking-order-id");
-//       const roomNumber = row.getAttribute("data-room-number");
-//       const userId = row.getAttribute("data-user-id");
-//       const checkIn = row.getAttribute("data-check-in");
-//       const checkOut = row.getAttribute("data-check-out");
-//       const petsAmount = row.getAttribute("data-pets-amount");
-//       const totalPrice = row.getAttribute("data-total-price");
-//       const ownerInstruction = row.getAttribute("data-owner-instruction");
-
-//       // Populate the modal with the data
-//       const bookingIdElement = modal!.querySelector(".booking-id");
-//       bookingIdElement!.textContent = bookingOrderId;
-//       const roomNumberElement = modal!.querySelector(".room-number");
-//       roomNumberElement!.textContent = roomNumber;
-//       const userIdElement = modal!.querySelector(".user-id");
-//       userIdElement!.textContent = userId;
-//       const checkInElement = modal!.querySelector(".check-in");
-//       checkInElement!.textContent = checkIn;
-//       const checkOutElement = modal!.querySelector(".check-out");
-//       checkOutElement!.textContent = checkOut;
-//       const petsAmountElement = modal!.querySelector(".pets-amount");
-//       petsAmountElement!.textContent = petsAmount;
-//       const totalPriceElement = modal!.querySelector(".total-price");
-//       totalPriceElement!.textContent = totalPrice;
-//       const ownerInstructionElement =
-//         modal!.querySelector(".owner-instruction");
-//       ownerInstructionElement!.textContent = ownerInstruction;
-//     });
-//   });
-// });
+const columns = [
+  {
+    key: "id",
+    label: "ID",
+  },
+  {
+    key: "roomNumber",
+    label: "Room Number",
+    sortable: true,
+  },
+  {
+    key: "userID",
+    label: "User ID",
+    sortable: true,
+  },
+  {
+    key: "checkIn",
+    label: "Check In",
+    sortable: true,
+    direction: "desc",
+  },
+  {
+    key: "checkOut",
+    label: "Check Out",
+    sortable: true,
+  },
+  {
+    key: "totalPrice",
+    label: "Total Price",
+    sortable: true,
+  },
+  {
+    key: "status",
+    label: "Status",
+    sortable: true,
+  },
+  {
+    key: "actions",
+    label: "Actions",
+  },
+];
 </script>
