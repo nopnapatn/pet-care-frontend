465c8f55c33326360c98804a872117cccfa36052
thyms-c <thanyamas.c@ku.th>
2023-11-07 00:52:49 +0700

    feat: other services form

--- pages/others/form.vue
+++ pages/others/form.vue
@@ -1,65 +1,302 @@
 <template>
-  <div>
-    <ol class="flex items-center w-full">
-      <li
-        class="flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800"
+  <div class="fix flex-col h-[46rem] justify-center px-48 py-10 gap-y-6">
+    <div class="flex w-full justify-center">
+      <ol class="flex justify-around w-full">
+        <li
+          class="flex w-full items-center text-yellow-500 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block"
+        >
+          <span
+            class="flex items-center justify-center w-10 h-10 bg-accent/40 rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-ui-radios"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M7 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-1zM0 12a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm7-1.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-1zm0-5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 8a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zM3 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm0 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
+              />
+            </svg>
+          </span>
+        </li>
+        <li
+          class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700"
+        >
+          <span
+            class="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0"
+          >
+            <svg
+              class="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 16"
+            >
+              <path
+                d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z"
+              />
+            </svg>
+          </span>
+        </li>
+        <li class="flex items-center">
+          <span
+            class="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              class="bi bi-credit-card-2-back-fill fill-gray-600"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5H0V4zm11.5 1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-2zM0 11v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-1H0z"
+              />
+            </svg>
+          </span>
+        </li>
+      </ol>
+    </div>
+    <div class="flex justify-center py-6 pb-8">
+      <select
+        name="pet-size"
+        id="pet-size"
+        class="select select-bordered bg-white focus:ring-primary focus:border-primary"
       >
-        <span
-          class="flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0"
-        >
-          <svg
-            class="w-3.5 h-3.5 text-blue-600 lg:w-4 lg:h-4 dark:text-blue-300"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 16 12"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 5.917 5.724 10.5 15 1.5"
+        <!-- <option disabled selected>What's your pet size?</option> -->
+        <option value="2">Small</option>
+        <option value="3">Medium</option>
+        <option value="4">Large</option>
+      </select>
+    </div>
+    <div class="grid grid-cols-3">
+      <!-- col 1 -->
+      <div class="flex flex-col px-10 gap-y-4">
+        <div class="flex flex-col gap-y-2">
+          <h1 class="text-lg">Select Pet</h1>
+          <div class="flex gap-4">
+            <input
+              type="radio"
+              name="radio-1"
+              class="text-secondary focus:ring-primary"
+              checked
             />
-          </svg>
-        </span>
-      </li>
-      <li
-        class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700"
-      >
-        <span
-          class="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0"
-        >
-          <svg
-            class="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 16"
-          >
-            <path
-              d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z"
+            <label for="radio1">Dog</label>
+          </div>
+          <div class="flex gap-4">
+            <input
+              type="radio"
+              name="radio-1"
+              class="text-secondary focus:ring-primary"
             />
-          </svg>
-        </span>
-      </li>
-      <li class="flex items-center w-full">
-        <span
-          class="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0"
-        >
-          <svg
-            class="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 18 20"
-          >
-            <path
-              d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v3H7V2Zm5.7 8.289-3.975 3.857a1 1 0 0 1-1.393 0L5.3 12.182a1.002 1.002 0 1 1 1.4-1.436l1.328 1.289 3.28-3.181a1 1 0 1 1 1.392 1.435Z"
+            <label for="radio1">Cat</label>
+          </div>
+        </div>
+        <div class="flex flex-col gap-y-2">
+          <h1 class="text-lg">Select Package</h1>
+          <div class="flex gap-4">
+            <input
+              type="radio"
+              name="radio-2"
+              class="text-secondary focus:ring-primary"
+              checked
             />
-          </svg>
-        </span>
-      </li>
-    </ol>
+            <label for="radio2">Spa Bath Package</label>
+          </div>
+          <div class="flex gap-4">
+            <input
+              type="radio"
+              name="radio-2"
+              class="text-secondary focus:ring-primary"
+              checked
+            />
+            <label for="radio3">All-Inclusive Groom Package</label>
+          </div>
+          <div class="flex gap-4">
+            <input
+              type="radio"
+              name="radio-2"
+              class="text-secondary focus:ring-primary"
+              checked
+            />
+            <label for="radio3">None</label>
+          </div>
+        </div>
+      </div>
+      <!-- col 2 -->
+      <div class="flex flex-col px-10">
+        <div class="flex flex-col gap-y-4 items-start mb-4">
+          <div class="flex flex-col gap-y-2 w-full">
+            <h1 class="text-lg">Select type</h1>
+            <div class="flex gap-4">
+              <input
+                type="radio"
+                name="radio-3"
+                class="text-secondary focus:ring-primary"
+                checked
+              />
+              <div class="flex justify-between w-full">
+                <label for="radio3">Trim Only</label>
+                <h1 class="text-md">$45</h1>
+              </div>
+            </div>
+            <div class="flex gap-4">
+              <input
+                type="radio"
+                name="radio-3"
+                class="text-secondary focus:ring-primary"
+                checked
+              />
+              <div class="flex justify-between w-full">
+                <label for="radio3">Complete Cut</label>
+                <h1>$60</h1>
+              </div>
+            </div>
+          </div>
+          <h1 class="text-lg">Select A La Carte Services</h1>
+          <!-- c1 -->
+          <div class="flex justify-between w-full">
+            <div class="flex items-center">
+              <input
+                id="checkbox-1"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-secondary bg-gray-100 border-gray-300 rounded focus:ring-primary dark:focus:ring-secondary dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="checkbox-1"
+                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >Brush Out</label
+              >
+            </div>
+            <h1>$45</h1>
+          </div>
+          <!-- c2 -->
+          <div class="flex justify-between w-full">
+            <div class="flex items-center">
+              <input
+                checked
+                id="checkbox-2"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-secondary bg-gray-100 border-gray-300 rounded focus:ring-primary dark:focus:ring-secondary dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="checkbox-2"
+                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >Breath Freshening</label
+              >
+            </div>
+            <h1>$45</h1>
+          </div>
+          <!-- c3 -->
+          <div class="flex justify-between w-full">
+            <div class="flex items-center">
+              <input
+                checked
+                id="checkbox-3"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-secondary bg-gray-100 border-gray-300 rounded focus:ring-primary dark:focus:ring-secondary dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="checkbox-3"
+                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >Nail Trim & File</label
+              >
+            </div>
+            <h1>$45</h1>
+          </div>
+          <!-- c4 -->
+          <div class="flex justify-between w-full">
+            <div class="flex items-center">
+              <input
+                checked
+                id="checkbox-4"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-secondary bg-gray-100 border-gray-300 rounded focus:ring-primary dark:focus:ring-secondary dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="checkbox-4"
+                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >Medicated Ear Cleaning</label
+              >
+            </div>
+            <h1>$45</h1>
+          </div>
+          <!-- c5 -->
+          <div class="flex justify-between w-full">
+            <div class="flex items-center">
+              <input
+                checked
+                id="checkbox-5"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-secondary bg-gray-100 border-gray-300 rounded focus:ring-primary dark:focus:ring-secondary dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="checkbox-5"
+                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >Flea Bath</label
+              >
+            </div>
+            <h1>$45</h1>
+          </div>
+          <!-- c6 -->
+          <div class="flex justify-between w-full">
+            <div class="flex items-center">
+              <input
+                checked
+                id="checkbox-6"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-secondary bg-gray-100 border-gray-300 rounded focus:ring-primary dark:focus:ring-secondary dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="checkbox-6"
+                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >Shed Reducing Treatment</label
+              >
+            </div>
+            <h1>$45</h1>
+          </div>
+          <!-- c7 -->
+          <div class="flex justify-between w-full">
+            <div class="flex items-center">
+              <input
+                checked
+                id="checkbox-7"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-secondary bg-gray-100 border-gray-300 rounded focus:ring-primary dark:focus:ring-secondary dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="checkbox-7"
+                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >Paw Polish</label
+              >
+            </div>
+            <h1>$45</h1>
+          </div>
+        </div>
+      </div>
+      <!-- col 3 -->
+      <div class="bg-primary/20 mx-6 px-8 h-fit pb-8 rounded-lg">
+        <div class="flex justify-between py-8">
+          <h1>Total</h1>
+          <h1>฿300</h1>
+        </div>
+        <NuxtLink
+          to="/others/report"
+          class="btn btn-primary text-white bg-secondary w-full"
+        >
+          Checkout
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
---
+++ pages/others/payment.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="fix flex-col h-[46rem] justify-center px-48 py-10 gap-y-6">
+    <div class="flex w-full justify-center">
+      <ol class="flex justify-around w-full">
+        <li
+          class="flex w-full items-center text-accent/40 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-accent/40 after:border-4 after:inline-block"
+        >
+          <span
+            class="flex items-center justify-center w-10 h-10 bg-accent/40 rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0"
+          >
+            <svg
+              class="w-3.5 h-3.5 text-blue-600 lg:w-4 lg:h-4 stroke-yellow-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 16 12"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5.917 5.724 10.5 15 1.5"
+              />
+            </svg>
+          </span>
+        </li>
+        <li
+          class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-accent/40 after:border-4 after:inline-block dark:after:border-gray-700"
+        >
+          <span
+            class="flex items-center justify-center w-10 h-10 bg-accent/40 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0"
+          >
+            <svg
+              class="w-3.5 h-3.5 text-blue-600 lg:w-4 lg:h-4 stroke-yellow-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 16 12"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5.917 5.724 10.5 15 1.5"
+              />
+            </svg>
+          </span>
+        </li>
+        <li class="flex items-center">
+          <span
+            class="flex items-center justify-center w-10 h-10 bg-accent/40 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              class="bi bi-credit-card-2-back-fill fill-yellow-500"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5H0V4zm11.5 1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-2zM0 11v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-1H0z"
+              />
+            </svg>
+          </span>
+        </li>
+      </ol>
+    </div>
+    <!-- content -->
+    <div class="grid grid-cols-2 justify-center mt-10">
+      <div class="w-3/4">
+        <div class="flex">
+          <img src="/images/QR.JPG" alt="" class="object-cover rounded-xl" />
+        </div>
+      </div>
+      <div class="h-full">
+        <div class="flex justify-between w-full px-10 py-6 border-b">
+          <h1>Total</h1>
+          <h1>$350</h1>
+        </div>
+        <div class="flex gap-6 mt-6">
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Full Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type here"
+              class="input input-bordered bg-white w-full max-w-xs"
+            />
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Last Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type here"
+              class="input input-bordered bg-white w-full max-w-xs"
+            />
+          </div>
+        </div>
+        <div class="flex gap-6 mt-4">
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Date Time</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type here"
+              class="input input-bordered bg-white w-full max-w-xs"
+            />
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Price</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type here"
+              class="input input-bordered bg-white w-full max-w-xs"
+            />
+          </div>
+        </div>
+        <div class="mt-10">
+          <input type="file" class="file-input bg-white w-full" />
+          <nuxt-link to="/" class="btn btn-primary text-white w-full mt-10"
+            >Confirm</nuxt-link
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
